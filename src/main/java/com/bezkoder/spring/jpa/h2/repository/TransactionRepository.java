170d395907f9aede96171c053d4e6b9e08bbdeac
Bertrand2808 <bertrand2808@icloud.com>
2024-02-07 12:32:32 +0100

    fix swagger issue

--- src/main/java/com/bezkoder/spring/jpa/h2/repository/TransactionRepository.java
+++ src/main/java/com/bezkoder/spring/jpa/h2/repository/TransactionRepository.java
@@ -2,7 +2,9 @@ package com.bezkoder.spring.jpa.h2.repository;
 
 import com.bezkoder.spring.jpa.h2.business.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
 }
